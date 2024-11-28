1060aa5d292a074c8ea11681b59e8187043f8059
nour eldin mahmoud <https://github.com/Nour5Eldin>
2024-11-28 23:56:29 +0200

    Initial Commit.

---
+++ app/src/main/java/com/noureldin/foodhub/ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.noureldin.foodhub.ui.theme
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFFFF6C3C)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val secondaryLight = Color(0xFFFE8160)
+val onSecondaryLight= Color(0xFF000000)
+val backgroundLight = Color(0xFFFFFFFF)
+val onBackgroundLight = Color(0xFF000000)
+val surfaceLight = Color(0xFF171D1E)
+val onSurfaceLight = Color(0xFF171D1E)
+
+val orange = Color(0xFFFF6C3C)
+val blue= Color(0xFF1A1D26)
+
+
+
+val primaryDark = Color(0xFF1A1D26)
+val onPrimaryDark = Color(0xFFFFFFFF)
+val secondaryDark = Color(0xFFFFB59E)
+val onSecondaryDark= Color(0xFFFFFFFF)
+val backgroundDark = Color(0xFF2A2F3D)
+val onBackgroundDark = Color(0xFFFFFFFF)
+val surfaceDark = Color(0xFFDEE3E5)
+val onSurfaceDark = Color(0xFFDEE3E5)
+
+
+
+
+
+
+
